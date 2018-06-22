a806828a13473c980f4e930f38b96b65c10c6c84
Keawa Rozet <keawa.rozet@yahoo.com>
2018-06-22 16:46:09 -0700

    KR initial project.

---
+++ app/src/main/java/com/apps/kb/myworkout/CreateWorkout.java
@@ -0,0 +1,54 @@
+package com.apps.kb.myworkout;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CreateWorkout extends AppCompatActivity {
+    private Button myWorkoutsButton;
+    private EditText nameMyWorkout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_workout);
+
+        myWorkoutsButton = findViewById(R.id.to_my_workouts);
+        myWorkoutsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMyWorkouts();
+            }
+        });
+        nameMyWorkout = findViewById(R.id.enter_workout_name);
+    }
+
+    public void openMyWorkouts() {
+        Intent intent = new Intent(this, MyWorkouts.class);
+        startActivity(intent);
+    }
+
+    public void writeWorkout(View view) {
+        String name = nameMyWorkout.getText().toString();
+        String file_name = "my_workouts";
+        try {
+            FileOutputStream fileOutputStream = openFileOutput(file_name, MODE_PRIVATE);
+            fileOutputStream.write(name.getBytes());
+            fileOutputStream.close();
+            Toast.makeText(getApplicationContext(), "My workout saved", Toast.LENGTH_LONG).show();
+            nameMyWorkout.setText("");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/apps/kb/myworkout/MyWorkouts.java
@@ -0,0 +1,62 @@
+package com.apps.kb.myworkout;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MyWorkouts extends AppCompatActivity {
+    private Button createWorkoutButton;
+    private TextView displayMyWorkouts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_workouts);
+
+        createWorkoutButton = findViewById(R.id.to_create_workout);
+        createWorkoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreateWorkout();
+            }
+        });
+        displayMyWorkouts = findViewById(R.id.display_my_workouts);
+        displayMyWorkouts.setVisibility(View.GONE);
+    }
+
+    public void openCreateWorkout() {
+        Intent intent = new Intent(this, CreateWorkout.class);
+        startActivity(intent);
+    }
+
+    public void readMyWorkouts() {
+        try {
+            FileInputStream fileInputStream = openFileInput("my_workouts");
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuffer stringBuffer = new StringBuffer();
+            String name;
+
+            while ((name = bufferedReader.readLine()) != null) {
+                stringBuffer.append(name +"\n");
+            }
+
+            displayMyWorkouts.setText(stringBuffer.toString());
+            displayMyWorkouts.setVisibility(View.VISIBLE);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
