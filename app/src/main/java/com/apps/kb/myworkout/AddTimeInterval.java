979edbdcf20396b69a9f04d5b0794040a064a764
Keawa Rozet <keawa.rozet@yahoo.com>
2018-06-23 12:01:38 -0700

    KR adds new workout to my workouts.

---
+++ app/src/main/java/com/apps/kb/myworkout/AddTimeInterval.java
@@ -0,0 +1,13 @@
+package com.apps.kb.myworkout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddTimeInterval extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_time_interval);
+    }
+}
