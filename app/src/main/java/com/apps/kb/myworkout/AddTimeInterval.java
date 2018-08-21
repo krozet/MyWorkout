5785cf0e27d78de85398fd95edebe46379e8801b
Keawa Rozet <keawa.rozet@yahoo.com>
2018-08-21 14:33:58 -0700

    KR added background color selector.

--- app/src/main/java/com/apps/kb/myworkout/AddTimeInterval.java
+++ app/src/main/java/com/apps/kb/myworkout/AddTimeInterval.java
@@ -1,8 +1,11 @@
 package com.apps.kb.myworkout;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,22 +13,31 @@ import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.Toast;
 
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorChangedListener;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 
 import com.google.gson.Gson;
 
 public class AddTimeInterval extends AppCompatActivity {
-    Button addStartVoiceButton, addEndVoiceButton;
+    Button addStartVoiceButton, addEndVoiceButton, addBackgroundColorButton;
     TimeInterval timeInterval;
     NumberPicker minutesNumberPicker, secondsNumberPicker;
+    private int currentBackgroundColor = 0xffffffff;
+    private View root;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_time_interval);
+        root = findViewById(R.id.color_screen);
         timeInterval = new TimeInterval();
         timeInterval.setName("fuck brad");
 
+        // minutes and seconds
         minutesNumberPicker = findViewById(R.id.minutes_number_picker);
         secondsNumberPicker = findViewById(R.id.seconds_number_picker);
         setNumberPickers();
@@ -44,6 +56,7 @@ public class AddTimeInterval extends AppCompatActivity {
             }
         });
 
+        // start voice
         addStartVoiceButton = findViewById(R.id.start_voice);
         addStartVoiceButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,11 +65,47 @@ public class AddTimeInterval extends AppCompatActivity {
             }
         });
 
+        // end voice
         addEndVoiceButton = findViewById(R.id.end_voice);
         addEndVoiceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openAddTimeInterval();
+            }
+        });
+
+        // background color
+        addBackgroundColorButton = findViewById(R.id.background_color);
+        addBackgroundColorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Context context = AddTimeInterval.this;
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(AddTimeInterval.this);
+                ColorPickerDialogBuilder
+                        .with(context)
+                        .setTitle("Choose color")
+                        .initialColor(currentBackgroundColor)
+                        .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                        .density(12)
+                        .setOnColorSelectedListener(new OnColorSelectedListener() {
+                            @Override
+                            public void onColorSelected(int selectedColor) {
+                                toast("onColorSelected: 0x" + Integer.toHexString(selectedColor));
+                            }
+                        })
+                        .setPositiveButton("ok", new ColorPickerClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                                changeBackgroundColor(selectedColor);
+                            }
+                        })
+                        .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .build()
+                        .show();
             }
         });
     }
@@ -88,6 +137,15 @@ public class AddTimeInterval extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Voice Added", Toast.LENGTH_LONG).show();
             onBackPressed();
         }
+    }
+
+    private void toast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    private void changeBackgroundColor(int selectedColor) {
+        currentBackgroundColor = selectedColor;
+        root.setBackgroundColor(selectedColor);
     }
 
 }
