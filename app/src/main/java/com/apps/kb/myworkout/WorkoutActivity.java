99074ce93df1e0e29a817a4de78086585bacc79c
bradfoster333 <bradfoster333@gmail.com>
2018-08-24 16:58:25 -0700

    Revert "Started Adding Music Menu"
    This reverts commit a30eca189912f83a2f86fd9c5dea4fa4c0415ea0.

--- app/src/main/java/com/apps/kb/myworkout/WorkoutActivity.java
+++ app/src/main/java/com/apps/kb/myworkout/WorkoutActivity.java
@@ -1,7 +1,5 @@
 package com.apps.kb.myworkout;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -11,8 +9,7 @@ import android.widget.Toast;
 
 public class WorkoutActivity extends AppCompatActivity
 {
-    private Button editWorkout, startWorkout, selectMusic;
-    private String workoutName;
+    private Button editWorkout, startWorkout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,9 +18,7 @@ public class WorkoutActivity extends AppCompatActivity
         setContentView(R.layout.activity_workout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        workoutName = getIntent().getStringExtra("WORKOUT_NAME_ID");
-        getSupportActionBar().setTitle(workoutName);
+        getSupportActionBar().setTitle(getIntent().getStringExtra("WORKOUT_NAME_ID"));
 
         editWorkout = findViewById(R.id.edit_workout);
         editWorkout.setOnClickListener(new View.OnClickListener()
@@ -31,7 +26,8 @@ public class WorkoutActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                openEditWorkout();
+                Toast.makeText(getApplicationContext(),
+                        "This does nothing yet", Toast.LENGTH_LONG).show();
             }
         });
 
@@ -45,22 +41,6 @@ public class WorkoutActivity extends AppCompatActivity
                         "Lol you thought", Toast.LENGTH_LONG).show();
             }
         });
-
-        selectMusic = findViewById(R.id.music_button);
-        selectMusic.setOnClickListener(new View.OnClickListener()
-        {
-            @Override public void onClick(View v)
-            {
-
-            }
-        });
-    }
-
-    private void openEditWorkout()
-    {
-        Intent intent = new Intent(this, EditWorkoutActivity.class);
-        intent.putExtra("WORKOUT_NAME_ID", workoutName);
-        startActivity(intent);
     }
 
 }
